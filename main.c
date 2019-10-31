4d086d9a29a66a4db4f0b9988e66b896cdd847e5
dhleebb42 <55428726+dhleebb42@users.noreply.github.com>
2019-10-31 16:13:07 +0900

    09_1031(2)

--- main.c
+++ main.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	
-	int i;
-	int a[5] = {1,2,3,4,5};
-	int b[5] = {1,2,3,4,5};
-	
-	for (i=0;i<5;i++)
-	{
-		if (a[i] != b[i])
-		{
-			printf("&i is different\n", i);
-		}
-	
+
+void printMatrix(int a[][COLS]){
+	int i, j;
+	for (i=0;i<ROWS;i++) {
+		for (j=0;j<COLS;j++)
+			printf("%i", a[i][j]);
+		printf("\n");
 	}
+}
+void addMatrix(int a[][COLS], int b[][COLS], int c[][COLS]){
+	int i, j;
+	for (i=0;i<ROWS;i++)
+		for(j=0;j<COLS;j++)
+			c[i][j] = a[i][j]+b[i][j]; 
+}
+	
+int main(int argc, char *argv[]) 
+{
+	int a[ROWS][COLS]={ 
+		{2,3,0},
+		{8,9,1},
+		{7,0,5}};
+	int b[ROWS][COLS] = {  
+		{1,0,0},
+		{0,1,0},
+		{0,0,1}};
+	int c[ROWS][COLS]; 
+		
+	addMatrix(a,b,c);
+	printMatrix(c);	 
 	
 	return 0;
 }
